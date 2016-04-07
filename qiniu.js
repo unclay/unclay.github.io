8b865f037f55da760deeaa828de7d3ee491a5e0e
unclay <wcl58981298@gmail.com>
2016-04-07 14:00:29 +0800

    qiniu upload

--- qiniu.js
+++ qiniu.js
@@ -1,8 +1,10 @@
+'use strict';
 var qiniu = require('qiniu');
 
 var co = require('co');
 var thunkify = require('thunkify');
 var fs = require('fs');
+var path = require('path');
 
 //需要填写你的 Access Key 和 Secret Key
 qiniu.conf.ACCESS_KEY = '6Pdy-xSflb1yG59sz-KZEb2EYsE8IJCyO45UhCMc';
@@ -10,10 +12,10 @@ qiniu.conf.SECRET_KEY = 'atJWuH-P_MScJs-XZyX2emsO4GJmwy95tP7bmGC5';
 
 
 //要上传的空间
-bucket = 'source';
+let bucket = 'source';
 
 //上传到七牛后保存的文件名
-key = 'common/js/vue123.js';
+let key = 'common/js/vue123.js';
 
 //构建上传策略函数
 function uptoken(bucket, key) {
@@ -21,20 +23,56 @@ function uptoken(bucket, key) {
   return putPolicy.token();
 }
 
-//生成上传 Token
-token = uptoken(bucket, key);
+const C_dirs = 'admin common demo moe'.split(' ');
 
-//要上传文件的本地路径
-filePath = './common/js/vue.js'
+function loop(path, cb){
+	co(function *(){
+		let stat = yield thunkify(fs.stat)(path);
+		if( stat.size === 0 ){
+			cb(null, yield thunkify(fs.readdir)(path));
+		} else {
+			cb(null, [path]);
+		}
+	}).catch(cb);
+}
+function loopPath(paths, cb){
+	co(function *(){
+		let statics = [];
+		let __paths = [];
+		for(let item of paths){
+			let dirs = yield thunkify(fs.readdir)(item);
+			for(let filepath of dirs){
+				let stat = yield thunkify(fs.stat)(path.join(item, filepath));
+				if( stat.isFile() ){
+					statics.push(path.join(item, filepath));
+				} else {
+					__paths.push(path.join(item, filepath));
+				}
+			}
+		}
+		if( __paths.length > 0 ) statics = statics.concat( yield thunkify(loopPath)(__paths) );
+		cb(null, statics);
+	}).catch(cb);
+}
 
 //调用uploadFile上传
 // uploadFile(token, key, filePath);
 co(function *(){
-	var dirs = yield thunkify(fs)
 
-	var extra = new qiniu.io.PutExtra();
-	var ret = (yield thunkify(qiniu.io.putFile)(token, key, filePath, extra))[0];
-	 console.log(ret.hash, ret.key, ret.persistentId);
+	for(let i=0; i<C_dirs.length; i++){
+		C_dirs[i] = path.join(__dirname, C_dirs[i]);
+	}
+	// console.log( C_dirs );
+	let paths = yield thunkify(loopPath)(C_dirs);
+	for(let item of paths){
+		let key = item.replace(__dirname + '\\', '').replace(/\\/gi, '/');
+		let token = uptoken(bucket, key);
+		var extra = new qiniu.io.PutExtra();
+		var ret = (yield thunkify(qiniu.io.putFile)(token, key, item, extra))[0];
+	}
+	// var dirs = yield thunkify(fs.stat)('./common.qq');
+	// console.log(dirs);
+	
 }).catch(function(err){
 	console.log(err);
 });
